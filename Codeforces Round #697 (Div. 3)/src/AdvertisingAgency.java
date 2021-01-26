10ed8bb7f7d0fea8d78dc58c2bdc346ed764dab7
Adarsh Goswami <adarsh010120@gmail.com>
2021-01-26 08:57:38 +0530

    26 jan

---
+++ Codeforces Round #697 (Div. 3)/src/AdvertisingAgency.java
@@ -0,0 +1,226 @@
+import java.util.*;
+import java.io.*;
+
+// TODO: 26th January 2021
+
+public class AdvertisingAgency {
+    InputStream is;
+    PrintWriter out;
+    String INPUT = "";
+    int MOD= 1000000007;
+
+    void solve() throws IOException {
+        int t = ni();
+        for (int ii = 0; ii < t; ii++) {
+            int n= ni(), k= ni();
+
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < n; i++)
+            {
+                int key= ni();
+                map.put(key, map.getOrDefault(key, 0)+ 1);
+            }
+
+            List<Integer> arr= new ArrayList<>(map.keySet());
+            Collections.sort(arr, Collections.reverseOrder());
+
+            long ans= 1;
+            int i= 0;
+            while(k!= 0){
+                int key= arr.get(i);
+                int val= map.get(key);
+
+                if(val<= k)
+                    k-= val;
+                else{
+
+                    break;
+                }
+                i++;
+            }
+
+            out.println(ans);
+        }
+    }
+
+    long power(int x, int y, int p)
+    {
+
+        // Initialize result
+        long res = 1;
+
+        // Update x if it is more than or
+        // equal to p
+        x = x % p;
+
+        while (y > 0) {
+
+            // If y is odd, multiply x
+            // with result
+            if (y % 2 == 1)
+                res = (res * x) % p;
+
+            // y must be even now
+            y = y >> 1; // y = y/2
+            x = (x * x) % p;
+        }
+
+        return res;
+    }
+
+    // Returns n^(-1) mod p
+    long modInverse(int n, int p)
+    {
+        return power(n, p - 2, p);
+    }
+
+    // Returns nCr % p using Fermat's
+    // little theorem.
+    long nCrModPFermat(int n, int r,
+                             int p)
+    {
+
+        if (n<r)
+            return 0;
+        // Base case
+        if (r == 0)
+            return 1;
+
+        // Fill factorial array so that we
+        // can find all factorial of r, n
+        // and n-r
+        int[] fac = new int[n + 1];
+        fac[0] = 1;
+
+        for (int i = 1; i <= n; i++)
+            fac[i] = fac[i - 1] * i % p;
+
+        return (fac[n] * modInverse(fac[r], p)
+                % p * modInverse(fac[n - r], p)
+                % p)
+                % p;
+    }
+
+    void run() throws Exception {
+        is = INPUT.isEmpty() ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+        out = new PrintWriter(System.out);
+
+        solve();
+        out.flush();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new AdvertisingAgency().run();
+    }
+
+    private byte[] inbuf = new byte[1024];
+    public int lenbuf = 0, ptrbuf = 0;
+
+    private int readByte() {
+        if (lenbuf == -1) throw new InputMismatchException();
+        if (ptrbuf >= lenbuf) {
+            ptrbuf = 0;
+            try {
+                lenbuf = is.read(inbuf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (lenbuf <= 0) return -1;
+        }
+        return inbuf[ptrbuf++];
+    }
+
+    private boolean isSpaceChar(int c) {
+        return !(c >= 33 && c <= 126);
+    }
+
+    private int skip() {
+        int b;
+        while ((b = readByte()) != -1 && isSpaceChar(b)) ;
+        return b;
+    }
+
+    private double nd() {
+        return Double.parseDouble(ns());
+    }
+
+    private char nc() {
+        return (char) skip();
+    }
+
+    private String ns() {
+        int b = skip();
+        StringBuilder sb = new StringBuilder();
+        while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+
+    private char[] ns(int n) {
+        char[] buf = new char[n];
+        int b = skip(), p = 0;
+        while (p < n && !(isSpaceChar(b))) {
+            buf[p++] = (char) b;
+            b = readByte();
+        }
+        return n == p ? buf : Arrays.copyOf(buf, p);
+    }
+
+    private char[][] nm(int n, int m) {
+        char[][] map = new char[n][];
+        for (int i = 0; i < n; i++) map[i] = ns(m);
+        return map;
+    }
+
+    private int[] na(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = ni();
+        return a;
+    }
+
+    private int ni() {
+        int num = 0, b;
+        boolean minus = false;
+        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-')) ;
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+
+        while (true) {
+            if (b >= '0' && b <= '9') {
+                num = num * 10 + (b - '0');
+            } else {
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+
+    private long nl() {
+        long num = 0;
+        int b;
+        boolean minus = false;
+        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-')) ;
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+
+        while (true) {
+            if (b >= '0' && b <= '9') {
+                num = num * 10 + (b - '0');
+            } else {
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+
+    private void tr(Object... o) {
+        if (INPUT.length() > 0) System.out.println(Arrays.deepToString(o));
+    }
+}
+
