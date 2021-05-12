5316af3f1b74a94f4d2414dffccc3e5addc1db6c
Adarsh Goswami <adarsh010120@gmail.com>
2021-05-12 12:06:57 +0530

    12th May

---
+++ RANDOM 1100+/src/MinimumTriangulation.java
@@ -0,0 +1,120 @@
+import java.util.*;
+import java.io.*;
+
+public class MinimumTriangulation {
+    PrintWriter out;
+    StringTokenizer st;
+    BufferedReader br;
+    final int imax = Integer.MAX_VALUE, imin = Integer.MIN_VALUE;
+    final int mod = 1000000007;
+
+    /**
+     *
+     */
+
+    void solve() throws Exception {
+        int  n= ni();
+        int ans= 0;
+        for(int i=2;i<n;i++) ans+= (i+1)*i;
+
+        out.println(ans);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new MinimumTriangulation().run();
+    }
+
+    void run() throws Exception {
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            File file = new File("C:\\college\\CodeForces\\inputf.txt");
+            br = new BufferedReader(new FileReader(file));
+            out = new PrintWriter("C:\\college\\CodeForces\\outputf.txt");
+        } else {
+            out = new PrintWriter(System.out);
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        st = new StringTokenizer("");
+        while (true) {
+            solve();
+            String s = br.readLine();
+            if (s == null) break;
+            else st = new StringTokenizer(s);
+        }
+        out.flush();
+    }
+
+    void read() throws Exception {
+        st = new StringTokenizer(br.readLine());
+    }
+
+    int ni() throws Exception {
+        if (!st.hasMoreTokens())
+            read();
+
+        return Integer.parseInt(st.nextToken());
+    }
+
+    long nl() throws Exception {
+        if (!st.hasMoreTokens())
+            read();
+
+        return Long.parseLong(st.nextToken());
+    }
+
+    double nd() throws Exception {
+        if (!st.hasMoreTokens())
+            read();
+
+        return Double.parseDouble(st.nextToken());
+    }
+
+    String ns() throws Exception {
+        String s = br.readLine();
+        return s.length() == 0 ? br.readLine() : s;
+    }
+
+    void print(int[] arr) {
+        for (int i : arr)
+            out.print(i + " ");
+        out.println();
+    }
+
+    void print(long[] arr) {
+        for (long i : arr)
+            out.print(i + " ");
+        out.println();
+    }
+
+    void print(int[][] arr) {
+        for (int[] i : arr) {
+            for (int j : i)
+                out.print(j + " ");
+            out.println();
+        }
+    }
+
+    void print(long[][] arr) {
+        for (long[] i : arr) {
+            for (long j : i)
+                out.print(j + " ");
+            out.println();
+        }
+    }
+
+    long add(long a, long b) {
+        if (a + b >= mod)
+            return (a + b) - mod;
+        else
+            return a + b >= 0 ? a + b : a + b + mod;
+    }
+
+    long mul(long a, long b) {
+        return (a * b) % mod;
+    }
+
+    void print(boolean b) {
+        if (b) out.println("YES");
+        else out.println("NO");
+    }
+}
