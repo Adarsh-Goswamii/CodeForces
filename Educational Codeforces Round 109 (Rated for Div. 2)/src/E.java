c9c25fdc1639871d9ad1210ad4239367efb293b6
Adarsh Goswami <adarsh010120@gmail.com>
2021-05-18 18:34:59 +0530

    18th May

--- Educational Codeforces Round 109 (Rated for Div. 2)/src/E.java
+++ Educational Codeforces Round 109 (Rated for Div. 2)/src/E.java
@@ -1,51 +1,25 @@
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 import java.io.*;
 
-public class PersonalizedCup {
+public class E {
     PrintWriter out;
     StringTokenizer st;
     BufferedReader br;
     final int imax = Integer.MAX_VALUE, imin = Integer.MIN_VALUE;
     final int mod = 1000000007;
 
-    /**
-     *
-     */
-
     void solve() throws Exception {
         int t = 1;
-//        t = ni();
-
+        t = ni();
         for (int ii = 0; ii < t; ii++) {
-            char[] arr= ns().toCharArray();
-            int row= (int)Math.ceil(arr.length/20.0);
-            int col= (int)Math.ceil(arr.length*1.0/row);
 
-            int extra= row*col- arr.length;
-            char[][] ans= new char[row][col];
-//            for(int i=0;i<row;i++) {
-//                int temp= extra/ row;
-//                for(int j=0;j<temp;j++) ans[i][j]= '*';
-//            }
-
-            for(int i=0;i<extra- (extra/row)*row;i++) ans[i][col-1]= '*';
-
-            int index= 0;
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < col; j++) {
-                    if(ans[i][j]== '*') continue;
-                    ans[i][j]= arr[index++];
-                }
-            }
-
-            out.println(row+" "+col);
-            for(char[] i: ans)
-                out.println(new String(i));
         }
     }
 
     public static void main(String[] args) throws Exception {
-        new PersonalizedCup().run();
+        new E().run();
     }
 
     void run() throws Exception {
@@ -73,23 +47,22 @@ public class PersonalizedCup {
     }
 
     int ni() throws Exception {
-        if (!st.hasMoreTokens())
-            read();
-
+        if (!st.hasMoreTokens()) read();
         return Integer.parseInt(st.nextToken());
     }
 
-    long nl() throws Exception {
-        if (!st.hasMoreTokens())
-            read();
+    char nc() throws Exception {
+        if (!st.hasMoreTokens()) read();
+        return st.nextToken().charAt(0);
+    }
 
+    long nl() throws Exception {
+        if (!st.hasMoreTokens()) read();
         return Long.parseLong(st.nextToken());
     }
 
     double nd() throws Exception {
-        if (!st.hasMoreTokens())
-            read();
-
+        if (!st.hasMoreTokens()) read();
         return Double.parseDouble(st.nextToken());
     }
 
@@ -99,38 +72,32 @@ public class PersonalizedCup {
     }
 
     void print(int[] arr) {
-        for (int i : arr)
-            out.print(i + " ");
+        for (int i : arr) out.print(i + " ");
         out.println();
     }
 
     void print(long[] arr) {
-        for (long i : arr)
-            out.print(i + " ");
+        for (long i : arr) out.print(i + " ");
         out.println();
     }
 
     void print(int[][] arr) {
         for (int[] i : arr) {
-            for (int j : i)
-                out.print(j + " ");
+            for (int j : i) out.print(j + " ");
             out.println();
         }
     }
 
     void print(long[][] arr) {
         for (long[] i : arr) {
-            for (long j : i)
-                out.print(j + " ");
+            for (long j : i) out.print(j + " ");
             out.println();
         }
     }
 
     long add(long a, long b) {
-        if (a + b >= mod)
-            return (a + b) - mod;
-        else
-            return a + b >= 0 ? a + b : a + b + mod;
+        if (a + b >= mod) return (a + b) - mod;
+        else return a + b >= 0 ? a + b : a + b + mod;
     }
 
     long mul(long a, long b) {
@@ -142,10 +109,46 @@ public class PersonalizedCup {
         else out.println("NO");
     }
 
+    long binExp(long base, long power) {
+        long res = 1l;
+        while (power != 0) {
+            if ((power & 1) == 1) res = mul(res, base);
+            base = mul(base, base);
+            power >>= 1;
+        }
+        return res;
+    }
+
+    long gcd(long a, long b) {
+        if (b == 0) return a;
+        else return gcd(b, a % b);
+    }
+
+    // strictly smaller on left
+    static void stack_l(int[] arr, int[] left) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
+            if (stack.isEmpty()) left[i] = -1;
+            else left[i] = stack.peek();
+            stack.push(i);
+        }
+    }
+
+    // strictly smaller on right
+    static void stack_r(int[] arr, int[] right) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
+            if (stack.isEmpty()) right[i] = arr.length;
+            else right[i] = stack.peek();
+            stack.push(i);
+        }
+    }
+
     private void sort(int[] arr) {
         List<Integer> list = new ArrayList<>();
         for (int i : arr) list.add(i);
-
         Collections.sort(list);
         for (int i = 0; i < arr.length; i++) arr[i] = list.get(i);
     }
--- RANDOM 1100+/src/FortuneTelling.java
+++ RANDOM 1100+/src/FortuneTelling.java
@@ -1,28 +1,37 @@
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 import java.io.*;
 
-public class FoodOnThePlane {
+public class FortuneTelling {
     PrintWriter out;
     StringTokenizer st;
     BufferedReader br;
     final int imax = Integer.MAX_VALUE, imin = Integer.MIN_VALUE;
     final int mod = 1000000007;
 
-    /**
-     *
-     */
-
     void solve() throws Exception {
         int t = 1;
-        t = ni();
-
+//        t = ni();
         for (int ii = 0; ii < t; ii++) {
+            int n= ni();
+            int[] arr= new int[n];
+            for (int i = 0; i < n; i++) arr[i]= ni();
 
+            int min= 102, sum= 0;
+            for (int i = 0; i < n; i++) {
+                if(arr[i]%2== 1) min= Math.min(min, arr[i]);
+                sum+= arr[i];
+            }
+
+            if(sum%2== 1) out.println(sum);
+            else if((sum-min)%2== 1) out.println(sum- min);
+            else out.println(0);
         }
     }
 
     public static void main(String[] args) throws Exception {
-        new FoodOnThePlane().run();
+        new FortuneTelling().run();
     }
 
     void run() throws Exception {
@@ -50,23 +59,22 @@ public class FoodOnThePlane {
     }
 
     int ni() throws Exception {
-        if (!st.hasMoreTokens())
-            read();
-
+        if (!st.hasMoreTokens()) read();
         return Integer.parseInt(st.nextToken());
     }
 
-    long nl() throws Exception {
-        if (!st.hasMoreTokens())
-            read();
+    char nc() throws Exception {
+        if (!st.hasMoreTokens()) read();
+        return st.nextToken().charAt(0);
+    }
 
+    long nl() throws Exception {
+        if (!st.hasMoreTokens()) read();
         return Long.parseLong(st.nextToken());
     }
 
     double nd() throws Exception {
-        if (!st.hasMoreTokens())
-            read();
-
+        if (!st.hasMoreTokens()) read();
         return Double.parseDouble(st.nextToken());
     }
 
@@ -76,38 +84,32 @@ public class FoodOnThePlane {
     }
 
     void print(int[] arr) {
-        for (int i : arr)
-            out.print(i + " ");
+        for (int i : arr) out.print(i + " ");
         out.println();
     }
 
     void print(long[] arr) {
-        for (long i : arr)
-            out.print(i + " ");
+        for (long i : arr) out.print(i + " ");
         out.println();
     }
 
     void print(int[][] arr) {
         for (int[] i : arr) {
-            for (int j : i)
-                out.print(j + " ");
+            for (int j : i) out.print(j + " ");
             out.println();
         }
     }
 
     void print(long[][] arr) {
         for (long[] i : arr) {
-            for (long j : i)
-                out.print(j + " ");
+            for (long j : i) out.print(j + " ");
             out.println();
         }
     }
 
     long add(long a, long b) {
-        if (a + b >= mod)
-            return (a + b) - mod;
-        else
-            return a + b >= 0 ? a + b : a + b + mod;
+        if (a + b >= mod) return (a + b) - mod;
+        else return a + b >= 0 ? a + b : a + b + mod;
     }
 
     long mul(long a, long b) {
@@ -119,10 +121,46 @@ public class FoodOnThePlane {
         else out.println("NO");
     }
 
+    long binExp(long base, long power) {
+        long res = 1l;
+        while (power != 0) {
+            if ((power & 1) == 1) res = mul(res, base);
+            base = mul(base, base);
+            power >>= 1;
+        }
+        return res;
+    }
+
+    long gcd(long a, long b) {
+        if (b == 0) return a;
+        else return gcd(b, a % b);
+    }
+
+    // strictly smaller on left
+    static void stack_l(int[] arr, int[] left) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
+            if (stack.isEmpty()) left[i] = -1;
+            else left[i] = stack.peek();
+            stack.push(i);
+        }
+    }
+
+    // strictly smaller on right
+    static void stack_r(int[] arr, int[] right) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
+            if (stack.isEmpty()) right[i] = arr.length;
+            else right[i] = stack.peek();
+            stack.push(i);
+        }
+    }
+
     private void sort(int[] arr) {
         List<Integer> list = new ArrayList<>();
         for (int i : arr) list.add(i);
-
         Collections.sort(list);
         for (int i = 0; i < arr.length; i++) arr[i] = list.get(i);
     }
